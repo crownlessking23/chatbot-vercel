4b838dbaa4f0f5004f580f4c1521b196dd60b285
crownlessking23 <kpolo.a12345351@umak.edu.ph>
2025-09-20 22:21:26 +0800

    Add chatbot code, remove secrets

--- api/chat.js
+++ api/chat.js
@@ -5,7 +5,7 @@ export default async function handler(req, res) {
 
   try {
     const hfRes = await fetch(
-      "https://api-inference.huggingface.co/models/distilgpt2",
+      "https://api-inference.huggingface.co/models/gpt2",
       {
         method: "POST",
         headers: {
